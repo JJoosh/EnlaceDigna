f95f97145d3f7a4323ad1a7b6decba45feee9b95
LSKord <miguelchuc68@gmail.com>
2024-04-02 11:09:58 -0600

    antes de tratar de cambiar el css

--- front/src/components/galeria/galeria.vue
+++ front/src/components/galeria/galeria.vue
@@ -1,181 +1,342 @@
 <template lang="html">
-    <body>
-      <main>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <body>
+    <header id="header">
+        <img src="@/assets/recurso1.jpg" class="logo">
+      <input type="checkbox" id="menu">
+      <label for="menu">
+        <i class="fa-solid fa-bars menu_icono"></i>
+      </label>
+      <ul class="menu">
+        <li class="item">Ajustes</li>
+        <li class="item">Usuario</li>
+        <li class="btn">Salir</li>
+      </ul>
+    </header>
+    <main>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-      <div class="main-container">
-        <!-- <div class="overlay">
-          <div class="slideshow">
-            <span class="btn-cerrar">&times;</span>
-            <div class="botones adelante">
-              <i class="fa-solid fa-circle-arrow-right"></i>
-            </div>
-            <div class="botones atras">
-              <i class="fa-solid fa-circle-arrow-left"></i>
-            </div>
-            <img src="" alt="" id="img-siledeshow">
+    <div class="main-container">
+      <!-- <div class="overlay">
+        <div class="slideshow">
+          <span class="btn-cerrar">&times;</span>
+          <div class="botones adelante">
+            <i class="fa-solid fa-circle-arrow-right"></i>
           </div>
-        </div> -->
-        <div class="recent-orders">
-          <div class="box-date">
+          <div class="botones atras">
+            <i class="fa-solid fa-circle-arrow-left"></i>
+          </div>
+          <img src="" alt="" id="img-siledeshow">
+        </div>
+      </div> -->
+      <div class="recent-orders">
+        <div class="box-date">
           <h2>Ultrasonido: Nombre</h2>
           <p>Fecha: 1/04/2023</p>
         </div>
-                    <div class="container" >
-                      hola
-                      <div href="grid" class="grid-container">
-                      </div>
-                    </div>
-                    
+        <div class="container" >
+          <div href="grid" class="grid-container">
+            <img class="grid-item" src="https://picsum.photos/230/155.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/152.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/153.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/154.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/150.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/158.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/153.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/150.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/152.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/153.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/154.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/150.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/158.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/153.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/155.jpg" width="230" height="155"/>
+          </div>
+        </div> 
+        <div class="box-date">
+          <h2>Ultrasonido: Nombre1</h2>
+          <p>Fecha: 1/04/2023</p>
         </div>
+        <div class="container" >
+          <div href="grid" class="grid-container">
+                          <img class="grid-item" src="https://picsum.photos/230/155.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/152.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/153.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/154.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/150.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/158.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/153.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/150.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/152.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/153.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/154.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/150.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/158.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/153.jpg" width="230" height="155"/>
+                          <img class="grid-item" src="https://picsum.photos/230/155.jpg" width="230" height="155"/>
+                          
+    <!-- <img class="grid-item" src="https://picsum.photos/230/300.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/410.jpg" />
+    <img class="grid-item" src="https://picsum.photos/230/250.jpg" />
+    <img class="grid-item" src="https://picsum.photos/230/180.jpg" />
+    <img class="grid-item" src="https://picsum.photos/230/340.jpg" />
+    <img class="grid-item" src="https://picsum.photos/230/120.jpg" />
+    <img class="grid-item" src="https://picsum.photos/230/215.jpg"  /> -->
+    <!-- <img class="grid-item" src="https://picsum.photos/230/420.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/320.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/153.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/480.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/260.jpg" />
+    <img class="grid-item" src="https://picsum.photos/230/450.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/135.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/180.jpg" />
+    <img class="grid-item" src="https://picsum.photos/230/340.jpg" />
+    <img class="grid-item" src="https://picsum.photos/230/120.jpg" />
+    <img class="grid-item" src="https://picsum.photos/230/215.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/420.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/320.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/153.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/480.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/260.jpg" />
+    <img class="grid-item" src="https://picsum.photos/230/450.jpg"  />
+    <img class="grid-item" src="https://picsum.photos/230/135.jpg"  /> -->
+                    </div>
+                  </div>
       </div>
-      </main>
+      
+    </div>
+    </main>
 </body>
 </template>
 <style>
-main .recent-orders .container:hover{
-  box-shadow: none;
-  background-color: #fff;
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;800&display=swap');
+
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'Nunito', sans-serif;
+  font-size: 20px;
+  list-style: none;
 }
- main .recent-orders .container{
-        background-color: #fff;
-        width: 100%;
-        padding: 1.8rem;
-        text-align: center;
-        box-shadow:  0 2rem 3rem rgba(132, 139, 200, 0.18);
-        border-radius: 20px;
-        transition: all 0.3s ease;
-        display: flex;
-        justify-content: space-around;
-        flex-wrap: wrap;
-        gap: 1.4rem;
-        cursor: pointer;
-        
-        align-items: left;
-        justify-content: left;
-    }
- *{
-        margin: 0;
-        padding: 0;
-        outline: 0;
-        appearance: 0;
-        border: 0;
-        text-decoration: none;
-        box-sizing: border-box;
-        background-color: #f6f6f9;
-    }
-    body{
-      font-family: 'Poppins', sans-serif;
-      width: 100vw;
-        height: 100vh;
-        font-size: 0.88rem;
-        user-select: none;
-        overflow-x: hidden;
-    }
-    .box-date{
-        margin: 20px 5%;
+header{
+width: 100%;
+position: fixed;
+display: flex;
+top: 0;
+left: 0;
+justify-content: space-between;
+align-items:center !important;
+padding: 0 5%;
+height: 100px;
+z-index: 1000000;
+}
+.logo{
+height: 70px;
+}
+.menu{
+display: flex;
+align-items: center;
+justify-content: center;
+}
+.item{
+margin: 0 15px;
+font-size: 18px;
+font-weight: 600;
+cursor: pointer;
+color: #000;
+}
+.item:hover{
+color: #33a451;
+}
+#menu{
+display: none;
+}
+.menu_icono{
+width: 25px;
+}
+label{
+cursor: pointer;
+display: none;
+}
+.btn{
+padding: 10px 24px;
+border-radius: 30px;
+color: #fff;
+background-color: #33a451;
+font-weight: 600;
+cursor: pointer;
+}
+main .recent-orders .container:hover{
+box-shadow: none;
+background-color: #fff;
+}
+main .recent-orders .container{
+      background-color: #fff;
+      width: 100%;
+      padding: 1.8rem;
+      text-align: center;
+      box-shadow:  0 2rem 3rem rgba(132, 139, 200, 0.18);
+      border-radius: 20px;
+      transition: all 0.3s ease;
       display: flex;
-      justify-content: space-between;
-    }
-    .main-container{
-      margin: 5%;
-      margin-top: 10px;
-    }
+      justify-content: space-around;
+      flex-wrap: wrap;
+      gap: 1.4rem;
+      cursor: pointer;
+      
+      align-items: left;
+      justify-content: left;
+  }
+*{
+      margin: 0;
+      padding: 0;
+      outline: 0;
+      appearance: 0;
+      border: 0;
+      text-decoration: none;
+      box-sizing: border-box;
+      background-color: #f6f6f9;
+  }
+  body{
+    font-family: 'Poppins', sans-serif;
+    width: 100vw;
+      height: 100vh;
+      font-size: 0.88rem;
+      user-select: none;
+      overflow-x: hidden;
+  }
+
+  
+  .box-date{
+      margin: 20px 5%;
+    display: flex;
+    justify-content: space-between;
+  }
+  .main-container{
+    margin: 5%;
+    margin-top: 10px;
+  }
 .box-date h2{
-  color: rgb(13, 14, 14);
+color: rgb(13, 14, 14);
 }
 .grid-container {
-  margin: 0 auto;
-  
+margin: 0 auto;
+
 }
 .grid-item {
-  width: 230px;
-  margin-bottom: 40px;
-  border-radius: 15px;
-  cursor: zoom-in;
-  background-color: lightgray;
-}
+ 
+/* width: 230px;  */
+margin-bottom: 40px;
+border-radius: 15px;
+cursor: zoom-in;
+background-color: lightgray;
+z-index: 4;
 
+}
 .grid-item:hover {
-  filter: opacity(0.9);
-  transform:scale(1.1);
-  box-shadow: 0 0 10px rgba(0, 0, 0, .5);
+filter: opacity(0.9);
+transform:scale(1.1);
+box-shadow: 0 0 10px rgba(0, 0, 0, .5);
 }
 .overlay{
-  position: fixed;
-  left: 0;
-  top:0;
-  width: 100vw;
-  height:100vh;
-  background-color: rgba(0, 0, 0, .5);
-  z-index: 100000;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transform-style: all .7s ease;
+position: fixed;
+left: 0;
+top:0;
+width: 100vw;
+height:100vh;
+background-color: rgba(0, 0, 0, .5);
+z-index: 100000;
+display: flex;
+justify-content: center;
+align-items: center;
+transform-style: all .7s ease;
 }
 .overlay .slideshow{
-  width: 90%;
-  height: 90%;
-  background-color: rgba(255, 255, 255, 0.9);
-  color: black;
-  position: relative;
-  display: flex;
-  justify-content: center;
+width: 90%;
+height: 90%;
+background-color: rgba(255, 255, 255, 0.9);
+color: black;
+position: relative;
+display: flex;
+justify-content: center;
 }
 .slideshow img{
-  height: 100%;
-  width: 100%;
-  object-fit: contain;
+height: 100%;
+width: 100%;
+object-fit: contain;
 }
 .btn-cerrar{
-  position: absolute;
-  top: -15px;
-  right: -15px;
-  font-size: 35px;
-  background-color: rgba(0, 0, 0, .7);
-  color: #fff;
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 5px;
-  transition: all .2s ease;
+position: absolute;
+top: -15px;
+right: -15px;
+font-size: 35px;
+background-color: rgba(0, 0, 0, .7);
+color: #fff;
+width: 30px;
+height: 30px;
+border-radius: 50%;
+display: flex;
+align-items: center;
+justify-content: center;
+padding: 5px;
+transition: all .2s ease;
 }
 .btn-cerrar :hover{
-  transform: scale(1,2);
-  cursor: pointer;
+transform: scale(1,2);
+cursor: pointer;
 }
 .botones{
-  width: 70px;
-  height: 70px;
-  border-radius: 50%;
-  color: #000;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 70px;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  cursor: pointer;
-  color: rgb(240, 106, 45);
+width: 70px;
+height: 70px;
+border-radius: 50%;
+color: #000;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 70px;
+position: absolute;
+top: 50%;
+transform: translateY(-50%);
+cursor: pointer;
+color: rgb(240, 106, 45);
 
 }
 .botones :hover>i{
-  transform: scale(1.1);
-  transition: all .2s ease;
+transform: scale(1.1);
+transition: all .2s ease;
 }
 .atras{
-  left: 20px;
+left: 20px;
 }
 .adelante{
-  right: 20px;
+right: 20px;
 }
 .mdi{
-  pointer-events: none;
+pointer-events: none;
+}
+@media (max-width: 900) {
+.menu{
+  padding: 30px;
+  margin: 0;
+}
+label{
+  display: initial;
+}
+.menu{
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  background-color: #474646;
+  display: none;
+}
+.item{
+  margin: 0 0 20px 0;
+  width: 100px;
+}
+#menu:checked ~.menu{
+display: initial;
+}
 }
 </style>
 <script>
@@ -184,23 +345,32 @@ import Masonry from 'masonry-layout';
 import imagesLoaded from 'imagesloaded';
 
 export default {
-  data() {
-    return {
-      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']
-    };
-  },
-  mounted() {
-    // Elemento que quieres convertir en un diseño de mampostería
-    var elem = document.querySelector('.grid-container');
-    imagesLoaded(elem,()=>{
-      var msnry = new Masonry( elem, {
-  // option
-        itemSelector: '.grid-item',
-        columnWidth: 230,
-        gutter: 20,
-        isFitWidth: true
+data() {
+  return {
+    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']
+  };
+},
+mounted() {
+  const header = document.getElementById("header");
+  window.addEventListener('scroll', () => {
+    var scroll = window.scrollY;
+    if (scroll > 10) {
+      header.style.backgroundColor = '#000';
+    } else {
+      header.style.backgroundColor = 'transparent';
+    }
+  });
+  // Elemento que quieres convertir en un diseño de mampostería
+  var elem = document.querySelector('.grid-container');
+  // imagesLoaded(elem,()=>{
+    var msnry = new Masonry( elem, {
+// option
+      itemSelector: '.grid-item',
+      columnWidth: 230,
+      gutter: 20,
+      isFitWidth: true
 });
-    });
-  }
+  // });
+}
 };
 </script>
